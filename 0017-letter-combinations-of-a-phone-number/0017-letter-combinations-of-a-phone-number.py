ebf145a9885ad0e0ba0eeecd6501e8d5c8fa2593
Jieun Jeong <jjesweet@naver.com>
2022-12-31 23:50:19 +0900

    Time: 36 ms (76.97%), Space: 14 MB (30.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,15 +2,8 @@ from itertools import product
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        d={}
-        d['2']=['a','b','c']
-        d['3']=['d','e','f']
-        d['4']=['g','h','i']
-        d['5']=['j','k','l']
-        d['6']=['m','n','o']
-        d['7']=['p','q','r','s']
-        d['8']=['t','u','v']
-        d['9']=['w','x','y','z']
+        d= {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'
+        }
         
         if not digits:
             return []
