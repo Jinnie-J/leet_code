94c560a34042a0f23149ab38be5bc9827775279a
Jieun Jeong <jjesweet@naver.com>
2023-03-05 13:39:14 +0900

    Time: 302 ms (72.78%), Space: 16.1 MB (97.20%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,27 @@
 class Solution:
-    def numIslands(self, grid):
-        if not grid:
-            return 0
-
+    def numIslands(self, grid: List[List[str]]) -> int:
+        dir = [[-1, 0], [1, 0], [0, 1], [0, -1]]  
         cnt = 0
+        queue = []
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
+                if grid[i][j] == "1":
                     cnt += 1
-        return cnt
-
-        
-    def dfs(self, grid, x, y):
-        dx=[1,0,-1,0]
-        dy=[0,1,0,-1]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
-                grid[nx][ny]='0'
-                self.dfs(grid,nx,ny)
-        
+                    queue.append([i, j])
+                    grid[i][j] = 0
+                    
+                while queue:
+                    x, y = queue.pop(0)
+                    
+                    for curr_dir in dir:
+                        nx = x + curr_dir[0]
+                        ny = y + curr_dir[1]
+                        
+                        if nx < 0 or ny < 0 or nx >= len(grid) or ny >= len(grid[0]):
+                            continue
+                            
+                        if grid[nx][ny] == "1":
+                            queue.append([nx, ny])
+                            grid[nx][ny] = 0
+        return cnt                    
