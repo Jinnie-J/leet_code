d8bfd077d88b5774b593744c94bd98d266b16e23
Jieun Jeong <jjesweet@naver.com>
2023-05-02 21:20:38 +0900

    Time: 48 ms (11.48%), Space: 16.6 MB (6.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        ans = []
+        answer = []
+        arr=[]
+        left=0
+        right=0
 
-        def backtrack(arr=[],left=0,right=0):
+        def backtrack(arr,left,right):
             if len(arr) == 2 * n:
-                ans.append("".join(arr))
+                answer.append("".join(arr))
                 return
-            if left < n: # '('가 가능하다면
+            if left < n:
                 arr.append("(")
                 backtrack(arr, left+1, right)
                 arr.pop()
-            if right < left: # ')'가 가능하다면
+            if right < left:
                 arr.append(")")
                 backtrack(arr, left, right+1)
                 arr.pop()
-        backtrack()
-        return ans
+
+        
+        backtrack(arr,left,right)
+        return answer
